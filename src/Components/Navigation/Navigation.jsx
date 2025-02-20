f2b5098f273fd051ebb364cae4580442b0fdcd4c
rocko678 <rocko678@gmail.com>
2025-02-20 22:33:23 +0000

    first changes

---
+++ src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navigation = () => {
+    return (
+        <nav style={{display: 'flex', justifycontent: 'flex-end'}}>
+            <p className='f3 link dim black underline pa3 pointer'>sign out</p>
+        </nav>
+    );
+    
+}
+
+export default Navigation;
