cc6ba757a811f2f8169f4c5a906b32f875072e81
rocko678 <rocko678@gmail.com>
2025-02-21 02:04:40 +0000

    bugspresent1

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navigation = () => {
     return (
-        <nav style={{display: 'flex', justifycontent: 'flex-end' }}>
+        <nav style={{ display: 'flex', justifycontent: 'flex-end' }}>
             <p className='f3 link dim black underline pa3 pointer'>sign out</p>
         </nav>
     );
